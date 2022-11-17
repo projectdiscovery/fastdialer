031ff42d0389ec73ba819ef580a5b8dba247d75f
Tarun Koyalwar <tarun@projectdiscovery.io>
2022-11-17 18:11:33 +0530

    updated REAME.md,gh action,example (closes #70)

--- example/main.go
+++ example/main.go
@@ -1,31 +1,3 @@
-# Fastdialer
-
-[![License](https://img.shields.io/github/license/projectdiscovery/fastdialer)](LICENSE.md)
-![Go version](https://img.shields.io/github/go-mod/go-version/projectdiscovery/fastdialer?filename=go.mod)
-[![Release](https://img.shields.io/github/release/projectdiscovery/fastdialer)](https://github.com/projectdiscovery/fastdialer/releases/)
-[![Checks](https://github.com/projectdiscovery/fastdialer/actions/workflows/build-test.yml/badge.svg)](https://github.com/projectdiscovery/fastdialer/actions/workflows/build-test.yml)
-[![GoDoc](https://pkg.go.dev/badge/projectdiscovery/fastdialer)](https://pkg.go.dev/github.com/projectdiscovery/fastdialer/fastdialer)
-
-
-Fastdialer is implementation of `net.Dialer` with lot of features like DNS Cache , Dial History etc
-
-# Features
-
-- DNS Cache
-- Dial History
-- Supports Memory/Hybrid/Disk Cache.
-- Supports Old/New TLS and x509 versions
-- Supports Resolution Using Hosts File
-- Cross Platform and more..
-
-For more details and documentation refer [GoDoc](https://pkg.go.dev/github.com/projectdiscovery/fastdialer/fastdialer).
-
-# Example
-
-An Example showing usage of fastdialer as a library is specified below:
-
-
-```go
 package main
 
 import (
@@ -79,4 +51,3 @@ func main() {
 	fmt.Println(jsonData)
 
 }
-```
